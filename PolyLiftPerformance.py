9e0d1c9d00199bc279daf3792c33ad21f41ff550
Adam Lloyd <adam@evolvedprojects.io>
2023-04-21 22:14:27 +1000

    Added comments

--- PolyLiftPerformance.py
+++ PolyLiftPerformance.py
@@ -7,32 +7,50 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 def main():
+    # Load the dataset from a CSV file
     print("Loading CSV file...")
     df = pd.read_csv('/Users/adamlloyd/Downloads/openpowerlifting.csv', low_memory=False)
     print("CSV file loaded.")
+
+    # Convert the BodyweightKg and TotalKg columns to numeric
     df['BodyweightKg'] = pd.to_numeric(df['BodyweightKg'])
     df['TotalKg'] = pd.to_numeric(df['TotalKg'])
+
+    # Drop rows with missing values in 'BodyweightKg' or 'TotalKg' columns
     df = df.dropna(subset=['BodyweightKg', 'TotalKg'])
     print(f"Number of rows after dropping missing values: {len(df):,}")
 
+    # Extract the relevant data
     X = df['BodyweightKg'].values.reshape(-1, 1)
     y = df['TotalKg'].values
+
+    # Train the polynomial regression model
     poly, model = train_polynomial_regression(X, y)
     return poly, model
 
 def train_polynomial_regression(X, y, degree=3):
+    # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Create the PolynomialFeatures instance and transform the data
     poly = PolynomialFeatures(degree=degree)
     X_train_poly = poly.fit_transform(X_train)
     X_test_poly = poly.transform(X_test)
+
+    # Fit the LinearRegression model
     model = LinearRegression()
     model.fit(X_train_poly, y_train)
+
     return poly, model
 
 def calculate_custom_points(body_weight, total_lifted, model, poly):
+    # Transform the bodyweight value and make a prediction
     body_weight_poly = poly.transform([[body_weight]])
     predicted_total = model.predict(body_weight_poly)[0]
+
+    # Calculate the points based on the ratio between the actual and predicted total
     custom_points = total_lifted / predicted_total * 500
+
     return custom_points
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -10,13 +10,16 @@ Abstract: This study proposes a novel approach to evaluate powerlifting performa
 
    2.1. Data Collection and Preprocessing We used a publicly available dataset containing powerlifting competition results. This dataset included information about athletes' body weight and total weight lifted (squat, bench press, and deadlift combined). We converted the body weight and total weight columns to numeric types and dropped any rows with missing values.
 
-   
-
    2.2. Model Development We employed a polynomial regression model with a degree of 3, as it provides a balance between model complexity and the risk of overfitting. The dataset was split into training (80%) and testing (20%) sets, and the model was fitted using the training data.
 
    2.3. Model Evaluation We evaluated the model's performance using mean squared error and R2 score for both training and testing sets. These metrics helped assess the model's ability to generalize and its overall accuracy in predicting the relationship between body weight and total weight lifted.
 
 3. Results: The custom polynomial regression model demonstrated improved performance compared to the Wilks Coefficient method. Our model had lower mean squared error and higher R2 scores for both training and testing sets. This indicates that the custom polynomial regression model can more accurately capture the relationship between body weight and total weight lifted.
+
+$$
+\text{Total Lifted} = \beta_0 + \beta_1 \cdot (\text{Bodyweight}) + \beta_2 \cdot (\text{Bodyweight})^2 + \beta_3 \cdot (\text{Bodyweight})^3 \\
+\text{Custom Points} = \frac{\text{Actual Total Lifted}}{\text{Predicted Total Lifted}} \cdot 500
+$$
 
 4. Discussion The proposed custom polynomial regression model offers several advantages over the Wilks Coefficient method:
 
